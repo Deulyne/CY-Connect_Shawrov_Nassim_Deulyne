d86dde1eed4143611720f40d4d747622836d38c0
Shawrov1 <130234608+Shawrov1@users.noreply.github.com>
2023-05-26 18:15:29 +0200

    Add files via upload

---
+++ affichage.h
@@ -0,0 +1,31 @@
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+
+#include <ncurses.h>
+
+
+void displayBoard(int **board, int boardNbLine, int boardNbColumn);
+
+void displayTokenChoicePosition(int *tokenChoiceBoard, int tokenChoicePosition,int boardNbColumn);
+
+void displayChoiceRotatePosition(int **board,int RotatePositionX,int RotatePositionY,int dimensioin,int boardNbLine,int boardNbColumn);
+
+void displayChoiceRotateDirection(int boardChoiceRotateDirection[2],int boardNbColumn);
+
+void winnerAnimation(int **board, int win_index[5][2],int actualToken,int boardNbLine,int boardNbColumn);
+
+void displayBegin(WINDOW*beginWin);
+
+void displayPause(WINDOW*pauseWin);
+
+void displaySave(WINDOW*saveWin);
+
+void displayLoading(WINDOW*loadingWin);
+
+void displayChoiceColWin(WINDOW* choiceColWin);
+
+void spacementY(int minusSpaceLine);
+
+void spacementX(int minusSpaceColumn);
+
+#endif
