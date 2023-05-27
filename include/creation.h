f6ec0e9fe616f33e65ec976d3c83a6378882eab6
Shawrov1 <130234608+Shawrov1@users.noreply.github.com>
2023-05-27 20:33:46 +0200

    Update creation.h

--- include/creation.h
+++ include/creation.h
@@ -6,6 +6,6 @@ void initialiseBoard(int **board,int boardNbLine,int boardNbColumn);
 
 void createChoiceBoardToken(int *tokenChoiceBoard,int boardNbColumn);
 
-void createChoiceBoardRotateDirection(int boardChoiceRotateDirection[2]);
+void createChoiceBoardRotateDirection(int boardChoiceRotateDirection[3]);
 
 #endif
